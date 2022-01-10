3dc808d2e899a7f2b17ce6d74ec396e849b0f96a
Grant Risk <riskgrant@gmail.com>
2022-01-10 11:35:11 -0500

    all links added, and started Team section

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload=function(){
     const discord = document.querySelector(".discord")
     const opensea = document.querySelector(".opensea")
     const twitter = document.querySelector(".twitter")
+    const armory = document.querySelector(".armory")
 
     discord.addEventListener("click", () => {
         window.open("https://discord.gg/AuvJpYWGQP", '_blank');
@@ -12,6 +13,10 @@ window.onload=function(){
     })
 
     twitter.addEventListener("click", () => {
-        window.open("google.com", '_blank');
+        window.open("https://twitter.com/spongybastards", '_blank');
+    })
+
+    armory.addEventListener("click", () => {
+        window.open("https://oncyber.io/the-spongy-bastard-armory", '_blank');
     })
 }
